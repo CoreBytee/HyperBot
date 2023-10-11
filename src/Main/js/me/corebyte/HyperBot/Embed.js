b399f2fdce02531a5cc186df73e7fca04cdb4502
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-10-11 15:29:36 +0200

    Create Embed.js

---
+++ src/Main/js/me/corebyte/HyperBot/Embed.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "title": "Current state: Dangerous",
+    "description": "Vanwege een probleem met Hypertubes in Satisfactory update 7 heb ik (CoreByte) een bot gemaakt die bijhoudt of het veilig is om met Hypertubes te travelen! (CoreByte is niet aansprakelijk voor fouten of glitches in deze bot)",
+    "url": "https://corebyte.me/",
+    "color": null,
+    "fields": [
+        {
+            "name": "Safe State",
+            "value": "In deze staat is het veilig om met hypertubes te travelen!",
+            "inline": true
+        },
+        {
+            "name": "Warning State",
+            "value": "In deze staat moet je ervoor zorgen dat je uit Hypertubes komt en/of niet te lange ritten start",
+            "inline": true
+        },
+        {
+            "name": "Dangerous State",
+            "value": "In deze staat is het niet veilig om met Hypertubes te travelen omdat de server elk moment een auto-save kan uitvoeren",
+            "inline": true
+        }
+    ],
+    "footer": {
+        "text": "De volgende save is"
+    },
+    "timestamp": "2023-10-11T13:23:00.000Z",
+    "thumbnail": {
+        "url": "https://cdn.discordapp.com/attachments/953214181082038282/1161653253684072468/row-1-column-2.png?ex=653914e6&is=65269fe6&hm=16dfd5bc353adc0ea7a7f546624fddfef22967ce9bac32a1b6ab5e926e05953d&"
+    }
+}
